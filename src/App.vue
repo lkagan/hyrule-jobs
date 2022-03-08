541539590bd551145f6e6de3baafb0d71abf502e
Larry Kagan <larry@superiocity.com>
2022-03-08 16:10:01 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app">Hello hyrule</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  components: { }
+});
+</script>
+
+<style>
+</style>
